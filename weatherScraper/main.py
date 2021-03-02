837ec993f83cf21d947eb5bd9f7cc918582c6712
Conor Kiy <conorkiy@gmail.com>
2021-03-02 19:41:32 +0000

    FEAT: Import packages

---
+++ weatherScraper/main.py
@@ -0,0 +1,14 @@
+"""
+Created on 01 March 2021
+
+@author: conorkiy@gmail.com
+"""
+
+
+import dbinfo
+import traceback
+import requests
+import datetime
+import sqlalchemy
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, Float, String, DateTime, insert
+
