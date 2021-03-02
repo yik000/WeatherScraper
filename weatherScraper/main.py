6321a84650010909576f64e142e897fa11ace627
Conor Kiy <conorkiy@gmail.com>
2021-03-02 19:48:31 +0000

    FEAT: create connection to database

--- weatherScraper/main.py
+++ weatherScraper/main.py
@@ -19,3 +19,21 @@ API variables
 WEATHER = "http://api.openweathermap.org/data/2.5/weather?"
 CITY = "Dublin,IE"
 
+
+"""
+Create engine - connect to database
+"""
+engine = create_engine("mysql+mysqlconnector://{}:{}@{}:3306/{}".format(dbinfo.USER, dbinfo.PASS, dbinfo.URI, dbinfo.DBNAME))
+
+meta = MetaData()
+
+weather = Table(
+    'weather', meta,
+    Column('main', String(256)),
+    Column('description', String(256)),
+    Column('temp', Integer),
+    Column('rainfall', Float),
+    Column('windspeed', Integer),
+    Column('time', DateTime)
+)
+
