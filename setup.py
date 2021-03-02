61c2f8385d845ae4a2c39b406d6a5057577b9421
Conor Kiy <conorkiy@gmail.com>
2021-03-02 20:24:28 +0000

    FIX: update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="weatherScraper",
-    version="1.0",
+    version="1.0.1",
     description="Hourly weather scraper for Dublin city",
     author="Conor Kiy",
     author_email="conorkiy@gmail.com",
